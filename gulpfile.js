2afedb6df14e222051ef0b5db3e7a8201034e2ad
DarkDragonkz <kirolosmarco2013@gmail.com>
2025-11-20 14:56:14 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
-const { Tasker } = require('paperback-extensions-common');
+const pb = require('paperback-extensions-common');
+
+// Cerca Tasker sia nella radice che dentro .default (per compatibilità TS)
+const Tasker = pb.Tasker || (pb.default && pb.default.Tasker);
+
+if (!Tasker) {
+    throw new Error('ERRORE CRITICO: Impossibile trovare Tasker dentro paperback-extensions-common. Le chiavi trovate sono: ' + JSON.stringify(Object.keys(pb)));
+}
 
 Tasker.bindTasks(exports);
